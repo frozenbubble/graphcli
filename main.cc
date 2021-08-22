0fbbe3b440455f240ef99b38eb1aa16c49a5fede
Peter Szucs <pszucs@outlook.com>
2021-08-22 20:12:36 +0200

    Upgrade to graphbuilder 0.0.3; add readme; Add error message for invalid_argument type errors

--- main.cc
+++ main.cc
@@ -75,6 +75,12 @@ int main(int argc, char* argv[])
 
         }        
     }
+    catch(const std::invalid_argument& e)
+    {
+        std::cerr << "Invalid input. See readme for examples." << std::endl;
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << std::endl;
